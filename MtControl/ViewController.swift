a464d7dafb8389c43c561a249c542e4b36f0fa44
Emilly Maia <emillypereira1290@gmail.com>
2022-08-23 09:54:40 -0300

    Initial Commit

---
+++ MtControl/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MtControl
+//
+//  Created by Emilly Maia on 23/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
